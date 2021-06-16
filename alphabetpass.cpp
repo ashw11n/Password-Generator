8b9444bdb3dd7fece1baa1bf2b3dfe675e41e4b9
Ashwin Hariharan <hariharan.ashwin44@gmail.com>
2021-06-16 00:02:45 -0400

    created alphabet and number versions
    created seperate alphabet based and integer based generators (to be made with GUI)

---
+++ alphabetpass.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <stdlib.h>
+
+using namespace std; 
+
+
+int main(){
+
+    //random letter generator
+    const char* const a_to_z = "abcdefghijklmnopqrstuvwxyz";
+    srand(time(0));
+    // generate random number i between 0 and 25 and then a_to_z[i] to gen random letter
+    vector<char> password(9);
+
+    for(int i = 0; i < password.size(); ++i){
+
+        int index = rand() % 26;
+        password.at(i) = a_to_z[index];
+    }
+    for(int j = 0; j < password.size(); ++j){
+        cout << password.at(j);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ password.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <ctime>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+
+    // random password generator: 9 characters
+    vector<char> overallPassword(9);
+    // character option groups as elements in array
+    const char* const characOptions[] = {"abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789"};
+
+    for(int i = 0; i < characOptions.size(); ++i){
+        for(int j = 0; j < overallPassword.size(); ++j){
+
+
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
